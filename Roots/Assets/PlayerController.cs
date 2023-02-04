af0e40653c87a29e4829a32f46d18ad7e6123c91
Plamena Atanasova <plamena.a1213@gmail.com>
2023-02-04 19:13:32 +0200

    Updated Player and Obstacle Controller

--- Roots/Assets/PlayerController.cs
+++ Roots/Assets/PlayerController.cs
@@ -7,17 +7,20 @@ public class PlayerController : MonoBehaviour
 
     public ObstacleController obstacleController;
     public float speed = 100f;
-
+    private Vector3 screenBoundries;
     // Start is called before the first frame update
     void Start() {
-
+        screenBoundries = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
     }
 
     // Update is called once per frame
     void Update() {
         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         mousePosition.z += Camera.main.nearClipPlane;
-        transform.position = Vector3.MoveTowards(transform.position, mousePosition, speed * Time.deltaTime);
+        Vector3 pos = Vector3.MoveTowards(transform.position, mousePosition, speed * Time.deltaTime);
+        pos.x = Mathf.Clamp(pos.x, (-1 * screenBoundries.x + 0.5f), screenBoundries.x - 0.5f);
+        pos.y = Mathf.Clamp(pos.y, (-1 * screenBoundries.y + 0.5f), screenBoundries.y - 0.5f);
+        transform.position = pos;
     }
     void OnCollisionEnter2D(Collision2D collider) {
         obstacleController.CollectedObstacle(collider.gameObject);
